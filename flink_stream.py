6708894d469605d5708f0052f5b5da315c4ec1a2
crawlersick <johnbyyang@gmail.com>
2021-01-21 15:50:18 +0800

    m

--- flink_stream.py
+++ flink_stream.py
@@ -2,9 +2,12 @@ from pyflink.common.serialization import SimpleStringEncoder
 from pyflink.common.typeinfo import Types
 from pyflink.datastream import StreamExecutionEnvironment
 from pyflink.datastream.connectors import StreamingFileSink
+import logging
 
 
 def tutorial():
+    logging.info("#################################11############################3")
+    print("#############################5555555555##########################################################33")
     env = StreamExecutionEnvironment.get_execution_environment()
     env.set_parallelism(1)
     ds = env.from_collection(
